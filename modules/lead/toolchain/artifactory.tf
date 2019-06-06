7f6f09580e4194dce80a9980ddea51f21dde6ed8
Michael Richard <michaelr@liatrio.com>
2019-06-06 15:05:04 -0700

    adding initial config import

--- modules/lead/toolchain/artifactory.tf
+++ modules/lead/toolchain/artifactory.tf
@@ -1,3 +1,30 @@
+data "template_file" "artifactory_security_values" {
+  template = "${file("${path.module}/artifactory.security.import.xml")}"
+}
+
+data "template_file" "artifactory_config_values" {
+  template = "${file("${path.module}/artifactory.config.import.xml")}"
+  // vars = {
+  //   namespace        = "${var.namespace}"
+  //   logstash_url     = "http://lead-dashboard-logstash.${var.namespace}.svc.cluster.local:9000"
+  // }
+}
+
+resource "kubernetes_config_map" "artifactory_config" {
+  metadata {
+    name = "lead-bootstrap-artifactory-config"
+  }
+
+  data {
+    artifactory.config.import.xml = "${data.template_file.artifactory_config_values.rendered}"
+  }
+
+  data {
+    security.import.xml = "${data.template_file.artifactory_security_values.rendered}"
+  }
+}
+
+
 
 resource "random_string" "artifactory_admin_password" {
   length  = 10
@@ -15,12 +42,20 @@ data "helm_repository" "jfrog" {
 }
 
 resource "helm_release" "artifactory" {
+  depends_on = ["kubernetes_config_map.artifactory_config"]
   repository = "jfrog"
   name       = "artifactory"
   namespace  = "${module.toolchain_namespace.name}"
   chart      = "artifactory"
   version    = "7.14.3"
   timeout    = 1200
+
+
+  #values = ["${data.template_file.artifactory_values.rendered}"]
+  set {
+    name  = "artifactory.configMapName"
+    value = "lead-bootstrap-artifactory-config"
+  }
 
   set {
     name  = "nginx.enabled"
