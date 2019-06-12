375913f13c20c9dbce3fd35a39be8221540575c0
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-06-12 11:40:10 -0700

    updating roles permissions

--- modules/common/namespace/main.tf
+++ modules/common/namespace/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_role" "tiller_role" {
   rule {
     api_groups = ["rbac.authorization.k8s.io"]
     resources = ["roles", "rolebindings"]
-    verbs = ["*"]
+    verbs = ["get", "create", "watch", "delete", "list"]
   }
 }
 
