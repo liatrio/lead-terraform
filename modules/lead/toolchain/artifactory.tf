6860e38976d3e8514feb712ef98a7b6265e89a2b
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-06-13 10:36:30 -0700

    removing duplicate modules

--- modules/lead/toolchain/artifactory.tf
+++ modules/lead/toolchain/artifactory.tf
@@ -11,19 +11,6 @@ data "template_file" "artifactory_config_values" {
   template = "${file("${path.module}/artifactory.config.import.xml.tpl")}"
 }
 
-module "toolchain_namespace" {
-  source     = "../../common/namespace"
-  namespace  = "${var.namespace}"
-  issuer_type = "${var.issuer_type}"
-  annotations {
-    name = "${var.namespace}"
-    cluster = "${var.cluster}"
-    "opa.lead.liatrio/ingress-whitelist" = "*.${var.namespace}.${var.cluster}.${var.root_zone_name}"
-    "opa.lead.liatrio/image-whitelist" = "${var.image_whitelist}"
-    "opa.lead.liatrio/elb-extra-security-groups" = "${var.elb_security_group_id}"
-  }
-}
-
 resource "kubernetes_secret" "artifactory_admin" {
   metadata {
     name      = "artifactory-admin-credential"
--- modules/lead/toolchain/nginx-ingress.tf
+++ modules/lead/toolchain/nginx-ingress.tf
@@ -1,16 +1,3 @@
-module "toolchain_namespace" {
-  source     = "../../common/namespace"
-  namespace  = "${var.namespace}"
-  issuer_type = "${var.issuer_type}"
-  annotations {
-    name = "${var.namespace}"
-    cluster = "${var.cluster}"
-    "opa.lead.liatrio/ingress-whitelist" = "*.${var.namespace}.${var.cluster}.${var.root_zone_name}"
-    "opa.lead.liatrio/image-whitelist" = "${var.image_whitelist}"
-    "opa.lead.liatrio/elb-extra-security-groups" = "${var.elb_security_group_id}"
-  }
-}
-
 data "template_file" "nginx_ingress_values" {
   template = "${file("${path.module}/nginx-ingress-values.tpl")}"
 
