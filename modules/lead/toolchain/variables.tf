f681c654d1e9b284f7aebc006bf5deea29f4b942
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-06-13 09:59:50 -0700

    Testing

--- modules/lead/toolchain/variables.tf
+++ modules/lead/toolchain/variables.tf
@@ -3,7 +3,9 @@ variable "root_zone_name" {}
 variable "cluster" {}
 variable "namespace" {}
 variable "image_whitelist" {}
-variable "issuer_type" {}
+variable "issuer_type" {
+    default = "selfSigned"
+  }
 variable "elb_security_group_id" {
     default = ""
 }
