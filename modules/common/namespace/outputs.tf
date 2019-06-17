e69dd62f197f4c50d9f15b8a7206994e4cca4bdb
Casey Lee <cplee@nektos.com>
2019-06-17 16:23:03 -0700

    Missing dep (#17)
    * add output dependency
    
    * fixing version

--- modules/common/namespace/outputs.tf
+++ modules/common/namespace/outputs.tf
@@ -3,4 +3,6 @@ output "name" {
 }
 output "tiller_service_account" {
     value = "${kubernetes_service_account.tiller_service_account.metadata.0.name}"
+
+    depends_on = ["kubernetes_role_binding.tiller_role_binding"]
 }
