1f04a41b1bf71b8032364d7b527473508818ee20
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-06-28 16:26:34 -0700

    missed an instance of password

--- stacks/environment-aws/istio.tf
+++ stacks/environment-aws/istio.tf
@@ -24,7 +24,6 @@ module "istio_system" {
   source             = "../../modules/common/istio"
   namespace          = "istio-system"
   crd_waiter         = "${null_resource.istio_init_delay.id}"
-  kiali_password     = "${var.kiali_password}"
   providers {
     helm = "helm.system"
   }
