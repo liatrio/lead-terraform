48a072c00110faa550b9416c4213054a9a2ea206
ltscif <ltscif>
2019-07-11 18:05:21 -0700

    tf .12 upgrade

--- modules/common/certificates/main.tf
+++ modules/common/certificates/main.tf
@@ -1,19 +1,20 @@
 data "template_file" "certificate_values" {
-  template = "${file("${path.module}/certificate-values.tpl")}"
+  template = file("${path.module}/certificate-values.tpl")
 
   vars = {
-    namespace = "${var.namespace}"
-    cluster_domain = "${var.cluster_domain}"
+    namespace      = var.namespace
+    cluster_domain = var.cluster_domain
   }
 }
-resource "helm_release" "certificates" {
-  count = "${var.enabled ? 1 : 0}"
-  name    = "certificates"
-  namespace = "${var.namespace}"
-  chart   = ".${replace(path.module, path.root, "")}/helm/certificates"
-  timeout = 600
-  wait    = true
 
-  values = ["${data.template_file.certificate_values.rendered}"]
+resource "helm_release" "certificates" {
+  count     = var.enabled ? 1 : 0
+  name      = "certificates"
+  namespace = var.namespace
+  chart     = ".${replace(path.module, path.root, "")}/helm/certificates"
+  timeout   = 600
+  wait      = true
+
+  values = [data.template_file.certificate_values.rendered]
 }
 
