3a3954c239e9db82cd7785fe833092a288300588
Casey Lee <casey@liatrio.com>
2019-07-15 00:10:39 -0700

    move dependency to namespace

--- modules/lead/toolchain/outputs.tf
+++ modules/lead/toolchain/outputs.tf
@@ -1,9 +1,9 @@
 output "namespace" {
   value = module.toolchain_namespace.name
+  depends_on = [kubernetes_cluster_role_binding.tiller_cluster_role_binding]
 }
 
 output "tiller_service_account" {
   value = module.toolchain_namespace.tiller_service_account
-  depends_on = [kubernetes_cluster_role_binding.tiller_cluster_role_binding]
 }
 
