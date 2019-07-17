aa8716830635c42bc13416b231ade8b47f5a7d1f
ltscif <ltscif>
2019-07-16 17:21:48 -0700

    Switch to using keycloak provider for creating realm

--- modules/lead/toolchain/keycloak.tf
+++ modules/lead/toolchain/keycloak.tf
@@ -1,30 +1,9 @@
-
 data "template_file" "keycloak_values" {
   template = file("${path.module}/keycloak-values.tpl")
 
   vars = {
     cluster_domain   = "${var.cluster}.${var.root_zone_name}"
     ingress_hostname = "keycloak.${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}"
-    admin_username   = kubernetes_secret.keycloak_admin.data.username
-    admin_password   = random_string.keycloak_admin_password.result
-    realm_name       = module.toolchain_namespace.name
-    realm_secret     = kubernetes_secret.keycloak_realm.metadata[0].name
-  }
-}
-
-data "template_file" "keycloak_realm" {
-  template = file("${path.module}/keycloak_realm.json")
-
-  vars = {
-    smtp_json = <<EOT
-{
-  "host": "mailhog",
-  "port": "1025",
-  "from": "keycloak@${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}",
-  "fromDisplayName": "Keycloak - ${module.toolchain_namespace.name}",
-  "auth": "false"
-}
-EOT
   }
 }
 
@@ -46,29 +25,6 @@ resource "kubernetes_secret" "keycloak_admin" {
   }
 }
 
-resource "kubernetes_secret" "keycloak_realm" {
-  metadata {
-    name      = "keycloak-realm"
-    namespace = module.toolchain_namespace.name
-  }
-  type = "Opaque"
-
-  data = {
-    "toolchain_realm.json" = data.template_file.keycloak_realm.rendered
-  }
-
-  lifecycle {
-    # once a realm has been imported, the only two options for future imports are
-    # IGNORE_EXISTING and OVERWRITE_EXISTING, and we don't want to ever overwrite a 
-    # realm since users would be lost, therefore don't apply any changes to the realm
-    # that might give the impression that the realm would be updated.
-    # we may want to look into using `kcadm.sh update realms` which will merge new
-    # attribute values with existing values, but `kcadm.sh` must be executed after startup
-    # and this helm chart doesn't expose a mechanism to run post-startup commands.
-    ignore_changes = [data]
-  }
-}
-
 resource "helm_release" "keycloak" {
   depends_on = [helm_release.mailhog]
   repository = data.helm_repository.codecentric.metadata[0].name
@@ -81,3 +37,35 @@ resource "helm_release" "keycloak" {
   values = [data.template_file.keycloak_values.rendered]
 }
 
+
+# while using client credentials is preferred, it would require initial client creation using the 
+# old realm import method, so just use password based setup since that is known prior to keycloak 
+# resource creation
+provider "keycloak" {
+  client_id = "admin-cli"
+  username  = "keycloak"
+  password  = random_string.keycloak_admin_password.result
+  url       = "https://keycloak.${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}"
+}
+
+resource "keycloak_realm" "realm" {
+  depends_on    = [helm_release.keycloak]
+  realm         = module.toolchain_namespace.name
+  enabled       = true
+  display_name  = title(module.toolchain_namespace.name)
+
+  registration_allowed            = true
+  registration_email_as_username  = true
+  reset_password_allowed          = true
+  remember_me                     = true
+  verify_email                    = true
+  login_with_email_allowed        = true
+  duplicate_emails_allowed        = false
+
+  smtp_server {
+    host              = "mailhog"
+    port              = "1025"
+    from              = "keycloak@${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}"
+    from_display_name = "Keycloak - ${title(module.toolchain_namespace.name)}"
+  }
+}
