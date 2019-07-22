b2ce62e9914a5f949cd3f30e0a8188f01ab8ea62
Grant Esparza <grante@liatrio.com>
2019-07-21 22:45:32 -0700

    Added list verb and updated role reference (#52)
    * Added list verb and updated role reference
    
    * Added deployments and extensions group

--- modules/lead/product/staging.tf
+++ modules/lead/product/staging.tf
@@ -130,9 +130,9 @@ resource "kubernetes_role" "default-staging-role" {
   }
 
   rule {
-    api_groups = [""]
-    resources  = ["pods", "jobs"]
-    verbs      = ["get"]
+    api_groups = ["", "extensions"]
+    resources  = ["pods", "jobs", "deployments"]
+    verbs      = ["get", "list"]
   }
 }
 
@@ -158,7 +158,7 @@ resource "kubernetes_role_binding" "default_staging_rolebinding" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
-    name      = "default"
+    name      = "default-staging-role"
   }
 
   subject {
