4e9b05f0f7bc0739235ef2a1eab863a315201915
Chris Schreiber <chriss@liatrio.com>
2019-07-24 16:14:40 -0700

    update toolchain resource limits

---
+++ modules/lead/toolchain/sonarqube-values.tpl
@@ -0,0 +1,7 @@
+resources:
+  requests:
+    cpu: 50m
+    memory: 1.5Gi
+  limits:
+    cpu: 200m
+    memory: 2Gi
--- modules/lead/toolchain/sonarqube.tf
+++ modules/lead/toolchain/sonarqube.tf
@@ -8,6 +8,10 @@ resource "random_string" "sonar_jenkins_password" {
   special = false
 }
 
+data "template_file" "sonarqube_values" {
+  template = file("${path.module}/sonarqube-values.tpl")
+}
+
 resource "helm_release" "sonarqube" {
   repository = "stable"
   name       = "sonarqube"
@@ -31,6 +35,8 @@ resource "helm_release" "sonarqube" {
     name  = "postgresql.postgresPassword"
     value = random_string.sonarqube_db_password.result
   }
+
+  values = [data.template_file.sonarqube_values.rendered]
 }
 
 resource "kubernetes_secret" "jenkins_sonar" {
@@ -56,4 +62,3 @@ resource "kubernetes_secret" "jenkins_sonar" {
     password = "admin"
   }
 }
-
