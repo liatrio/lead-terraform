a12ebb964c279a0895ab781eeddb23b6a4441043
Chris Schreiber <chriss@liatrio.com>
2019-08-06 17:16:50 -0700

    add dummy chart to force product tiller install

--- modules/lead/product/staging.tf
+++ modules/lead/product/staging.tf
@@ -16,6 +16,16 @@ module "staging_namespace" {
   }
 }
 
+resource "helm_release" "staging_product_init" {
+  name      = "product-init"
+  namespace = module.staging_namespace.name
+  chart     = "${path.module}/helm/product-init"
+  timeout   = 600
+  wait      = true
+
+  provider  = helm.staging
+}
+
 module "staging_certificate" {
   source = "../../common/certificates"
   namespace = "istio-system"
