23310660b8b146850d9a88a483de71fdbfc142d2
Chris Schreiber <chriss@liatrio.com>
2019-08-14 16:48:53 -0700

    add product stack for local env (#84)

--- stacks/environment-local/lead.tf
+++ stacks/environment-local/lead.tf
@@ -56,11 +56,11 @@ module "sdm" {
   product_version             = var.product_version
   slack_bot_token             = var.slack_bot_token
   slack_client_signing_secret = var.slack_client_signing_secret
-  workspace_role_name         = "local_workspace_role"  
+  workspace_role_name         = "local_workspace_role"
+  product_stack               = "product-local"  
 
   providers = {
     helm.system    = helm.toolchain
     helm.toolchain = helm.toolchain
   }
 }
-
