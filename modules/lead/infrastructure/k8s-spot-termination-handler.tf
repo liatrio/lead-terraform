d4571f60c5b5e2f08f1a1c786a1fc13cd590759b
Casey Lee <casey@liatrio.com>
2019-09-06 18:49:36 +0000

    allow k8s term handler on spot

--- modules/lead/infrastructure/k8s-spot-termination-handler.tf
+++ modules/lead/infrastructure/k8s-spot-termination-handler.tf
@@ -7,9 +7,8 @@ resource "helm_release" "k8s_spot_termination_handler" {
   name       = "k8s-spot-termination-handler"
   timeout    = 600
 
-  values = [var.ondemand_toleration_values]  
-  set {
-    name = "nodeSelector"
-    value = "kubernetes.io/lifecycle: spot"
+  set_string {
+    name  = "nodeSelector.kubernetes\\.io/lifecycle"
+    value = "spot"
   }
 }
