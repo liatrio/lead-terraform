4f2c165fdff6c0970a47b17407369671f4c0cc8f
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2019-10-07 17:20:05 -0700

    Add grafeas to cluster

--- modules/common/certificates/main.tf
+++ modules/common/certificates/main.tf
@@ -19,3 +19,10 @@ resource "helm_release" "certificates" {
   values = [data.template_file.certificate_values.rendered]
 }
 
+output "cert_status" {
+  value = helm_release.certificates[0].status
+}
+
+output "cert_name" {
+  value = var.name
+}
