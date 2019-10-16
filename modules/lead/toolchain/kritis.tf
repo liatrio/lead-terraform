14869bde272368c648f80860c39d475c4fba1a7f
Jonathon Rudy <31257900+JonRudy@users.noreply.github.com>
2019-10-16 15:05:25 -0700

    Eng 1205 (#136)
    * Adding kritis chart and deployment to cluster
    
    * trying to use module already existing
    
    * adding helm hook
    
    * breaking charts up for crd to be defined first
    
    * Removing unneeded files
    
    * removing folders
    
    * Updating tls secret name
    
    * Updating deployment namespace
    
    * fixing terraform error
    
    * adding back in install folders
    
    * updating config secret
    
    * removing preinstall and postinstall form helm charts and manually creating crd + validinghook resources
    
    * Updating module name to avoid duplication
    
    * commenting out post and pre install scripots
    
    * adding var to module
    
    * fixing typo
    
    * Creating depenedency on cert_status
    
    * removing delete pod
    
    * Fixing typo
    
    * Updating deploy yaml
    
    * fixing webook config definitions
    
    * Updating chart name
    
    * Adding helm-hooks and weights to svc account resources
    
    * Adding hook-failed into delete policy
    
    * fixing yaml errors
    
    * adding post delete to job
    
    * testing cabundle removal
    
    * reverting changes
    
    * adding debug of value
    
    * refactoring yaml resources
    
    * using actual value for ca.crt
    
    * reformatting lookup function
    
    * updating certificate
    
    * using decoded values
    
    * updating cert
    
    * refactoring
    
    * fixing missing space
    
    * updating to correct key
    
    * Trying double encoded values
    
    * adding tls.crt values
    
    * updating cert dns names
    
    * updating certificate names
    
    * updating ca bundle
    
    * fixxing webhooks names
    
    * testing outputs
    
    * encoding tls value
    
    * Helm uses cabundle values again
    
    * removing unneeded outputs
    
    * removing unused secret yaml and adding wait on certificate
    
    * adding wait on certifiacte
    
    * Using namespace variable

---
+++ modules/lead/toolchain/kritis.tf
@@ -0,0 +1,65 @@
+module "kritis_certificate" {
+  source = "../../common/certificates"
+
+  enabled = true
+  name = "kritis-cert"
+  namespace = var.namespace
+  domain = "kritis-validation-hook.${var.namespace}.svc"
+  acme_enabled = false
+  issuer_name = module.ca-issuer.name
+  certificate_crd = var.crd_waiter
+  wait_for_cert = true
+}
+
+resource "helm_release" "kritis-crd" {
+  name       = "kritis-crd"
+  namespace  = var.namespace
+  chart      = "${path.module}/charts/kritis-crd"
+  version    = "0.1.0"
+  timeout    = 600
+  wait       = true
+}
+
+data "kubernetes_secret" "kritis" {
+  depends_on = [module.kritis_certificate.cert_status]
+  metadata {
+    name = "${module.kritis_certificate.cert_name}-certificate"
+    namespace = var.namespace
+  }
+}
+
+output "caBundle" {
+  value = "${base64encode(lookup(data.kubernetes_secret.kritis.data, "tls.crt"))}"
+}
+
+resource "helm_release" "kritis" {
+  name       = "kritis-server"
+  namespace  = var.namespace
+  chart      = "${path.module}/charts/kritis-chart"
+  version    = "0.1.1"
+  timeout    = 600
+  wait       = true
+
+  depends_on = [module.kritis_certificate.cert_status, helm_release.kritis-crd]
+
+  set {
+    name = "caBundle"
+    value = "${base64encode(lookup(data.kubernetes_secret.kritis.data, "tls.crt"))}"
+  }
+
+  set {
+    name = "certificates.name"
+    value = "${module.kritis_certificate.cert_name}-certificate"
+  }
+
+  set {
+    name = "tlsSecretName" 
+    value = "${module.kritis_certificate.cert_name}-certificate"
+  }
+
+  set {
+    name = "serviceNamespace"
+    value = var.namespace
+  }
+}
+
