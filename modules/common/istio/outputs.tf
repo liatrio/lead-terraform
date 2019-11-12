28bfd789477b4b68963e1672c6067a1fb17b98b4
Michael Parker <michael@parker.gg>
2019-11-12 13:11:48 -0600

    remove unneeded output

--- modules/common/istio/outputs.tf
+++ modules/common/istio/outputs.tf
@@ -9,7 +9,3 @@ output "tiller_service_account" {
 output "cert_issuer_name" {
   value = module.istio_cert_issuer.issuer_name
 }
-
-output "cert_issuer_dns_provider_name" {
-  value = module.istio_cert_issuer.dns_provider_name
-}
