c1c3561bd45f91c0e1bedaa9454ae469e834f854
Jeremy Hayes <jeremyh@liatrio.com>
2019-12-09 15:29:17 -0500

    switch to prometheus operator

--- modules/lead/infrastructure/prometheus.tf
+++ modules/lead/infrastructure/prometheus.tf
@@ -2,16 +2,16 @@ data "template_file" "prometheus_values" {
   template = file("${path.module}/prometheus-values.tpl")
 }
 
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
+resource "helm_release" "prometheus_operator" {
+  name       = "prometheus-operator"
   namespace  = module.system_namespace.name
   repository = data.helm_repository.stable.metadata[0].name
-  chart      = "prometheus"
-  version    = "8.14.0"
+  chart      = "prometheus-operator"
+  version    = "8.3.3"
   timeout    = 600
   wait       = true
 
-  values = [data.template_file.prometheus_values.rendered, var.essential_toleration_values]
+  #values = [data.template_file.prometheus_values.rendered, var.essential_toleration_values]
 
   depends_on = [kubernetes_cluster_role_binding.tiller_cluster_role_binding]
 }
