ddcff5e42f0e89b27c7e7f650fbb80ed506260c7
Sean Jordan <seanj@liatrio.com>
2019-12-10 14:54:41 -0800

    Added password generation to grafana (#172)

--- modules/lead/toolchain/prometheus.tf
+++ modules/lead/toolchain/prometheus.tf
@@ -2,6 +2,13 @@ data "template_file" "prometheus_values" {
   template = file("${path.module}/prometheus-values.tpl")
 }
 
+resource "random_password" "password" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+
 resource "helm_release" "prometheus_operator" {
   name       = "prometheus-operator"
   namespace  = module.toolchain_namespace.name
@@ -10,6 +17,11 @@ resource "helm_release" "prometheus_operator" {
   version    = "8.3.3"
   timeout    = 600
   wait       = true
+  
+  set {
+    name = "grafana.adminPassword"
+    value = random_password.password.result
+  }
 
   #values = [data.template_file.prometheus_values.rendered, var.essential_toleration_values]
 
