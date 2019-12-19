0a19580c8e57492cfce55c81375ab463f82a4f59
Michael Parker <michael@parker.gg>
2019-12-19 13:49:01 -0600

    [eng-1418] integrate harbor with keycloak (#187)

--- modules/lead/toolchain/harbor.tf
+++ modules/lead/toolchain/harbor.tf
@@ -4,32 +4,32 @@ locals {
 }
 
 resource "random_string" "harbor_admin_password" {
-  length  = 10
+  length = 10
   special = false
 }
 
 resource "random_string" "harbor_db_password" {
-  length  = 10
+  length = 10
   special = false
 }
 
 resource "random_string" "harbor_secret_key" {
-  length  = 16
+  length = 16
   special = false
 }
 
 resource "random_string" "harbor_core_secret" {
-  length  = 16
+  length = 16
   special = false
 }
 
 resource "random_string" "harbor_jobservice_secret" {
-  length  = 16
+  length = 16
   special = false
 }
 
 resource "random_string" "harbor_registry_secret" {
-  length  = 16
+  length = 16
   special = false
 }
 
@@ -105,9 +105,11 @@ resource "helm_release" "harbor" {
   name = "harbor"
   namespace = module.toolchain_namespace.name
   chart = "harbor"
-  version = "1.2.3"
+  version = "1.3.0"
 
-  values = [data.template_file.harbor_values.rendered]
+  values = [
+    data.template_file.harbor_values.rendered
+  ]
 
   set_sensitive {
     name = "harborAdminPassword"
@@ -143,4 +145,65 @@ resource "helm_release" "harbor" {
     helm_release.harbor_certificates,
     helm_release.harbor_volumes
   ]
+}
+
+resource "keycloak_openid_client" "harbor_client" {
+  count = var.enable_harbor && var.enable_keycloak ? 1 : 0
+  realm_id = keycloak_realm.realm[0].id
+  client_id = "harbor"
+  name = "harbor"
+  enabled = true
+
+  access_type = "CONFIDENTIAL"
+  standard_flow_enabled = true
+
+  valid_redirect_uris = [
+    "https://${local.harbor_hostname}/c/oidc/callback"
+  ]
+
+  depends_on = [
+    helm_release.harbor
+  ]
+}
+
+resource "keycloak_openid_group_membership_protocol_mapper" "harbor_group_membership_mapper" {
+  count = var.enable_harbor && var.enable_keycloak ? 1 : 0
+  realm_id = keycloak_openid_client.harbor_client[0].realm_id
+  client_id = keycloak_openid_client.harbor_client[0].id
+  name = "harbor-group-membership-mapper"
+
+  claim_name = "groups"
+}
+
+resource "helm_release" "harbor_config" {
+  count = var.enable_harbor && var.enable_keycloak ? 1 : 0
+  chart = "${path.module}/charts/harbor-config"
+  name = "harbor-config"
+  namespace = module.toolchain_namespace.name
+  wait = true
+
+  set {
+    name = "harbor.username"
+    value = "admin"
+  }
+
+  set_sensitive {
+    name = "harbor.password"
+    value = random_string.harbor_admin_password.result
+  }
+
+  set {
+    name = "harbor.hostname"
+    value = local.harbor_hostname
+  }
+
+  set {
+    name = "keycloak.hostname"
+    value = local.keycloak_hostname
+  }
+
+  set_sensitive {
+    name = "keycloak.secret"
+    value = keycloak_openid_client.harbor_client[0].client_secret
+  }
 }
