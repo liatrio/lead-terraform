3a956e3a15ac0b58fce0865f27077afdc04f3363
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2020-02-10 10:41:36 -0800

    add count to harbor secret data source

--- modules/lead/product/harbor.tf
+++ modules/lead/product/harbor.tf
@@ -1,4 +1,5 @@
 data "kubernetes_secret" "product-harbor-creds" {
+  count = var.enable_harbor ? 1 : 0
   provider = kubernetes.toolchain
   metadata {
     name      = "${var.product_name}-harbor-credentials"
