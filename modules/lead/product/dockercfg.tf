0098be92e7e3e01923a1f2f38b5ab8a85cc391f2
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2020-02-10 12:31:24 -0800

    clean up template resource

--- modules/lead/product/dockercfg.tf
+++ modules/lead/product/dockercfg.tf
@@ -1,3 +1,9 @@
+locals {
+  artifactory_user = data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.username : ""
+  artifactory_pass = data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.password : ""
+  harbor_pass      = data.kubernetes_secret.product-harbor-creds[0] ? data.kubernetes_secret.product-harbor-creds[0].data.AUTH : ""
+}
+
 data "template_file" "dockercfg" {
   template = file("${path.module}/dockercfg.tpl")
  
@@ -5,11 +11,11 @@ data "template_file" "dockercfg" {
     email = "jenkins@liatr.io"
     artifactory_url   = "https://artifactory.toolchain.${var.cluster_domain}/docker-registry/${var.product_name}"
     artifactory_auth = base64encode(
-      "${data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.username : \"\"}:${data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.password : \"\"}",
+      "${local.artifactory_user}:${local.artifactory_pass}",
     )   
     harbor_url = "https://harbor.toolchain.${var.cluster_domain}/${var.product_name}"
     harbor_auth = base64encode(
-      "robot$imagepusher:${data.kubernetes_secret.product-harbor-creds[0] ? data.kubernetes_secret.product-harbor-creds[0].data.AUTH : \"\"}",
+      "robot$imagepusher:${local.harbor_pass}",
     )   
     enable_harbor = var.enable_harbor
   }
