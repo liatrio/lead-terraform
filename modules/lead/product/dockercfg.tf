824ff5a452dc07a665fb9ff6abc679bfd29ad522
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2020-02-10 12:25:28 -0800

    Combine into one template resource

--- modules/lead/product/dockercfg.tf
+++ modules/lead/product/dockercfg.tf
@@ -1,31 +1,16 @@
-data "template_file" "multi_dockercfg" {
-  count = var.enable_harbor ? 1 : 0
+data "template_file" "dockercfg" {
   template = file("${path.module}/dockercfg.tpl")
  
   vars = { 
     email = "jenkins@liatr.io"
     artifactory_url   = "https://artifactory.toolchain.${var.cluster_domain}/docker-registry/${var.product_name}"
     artifactory_auth = base64encode(
-      "${data.kubernetes_secret.jenkins_artifactory_credential[0].data.username}:${data.kubernetes_secret.jenkins_artifactory_credential[0].data.password}",
+      "${data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.username : \"\"}:${data.kubernetes_secret.jenkins_artifactory_credential[0] ? data.kubernetes_secret.jenkins_artifactory_credential[0].data.password : \"\"}",
     )   
     harbor_url = "https://harbor.toolchain.${var.cluster_domain}/${var.product_name}"
     harbor_auth = base64encode(
-      "robot$imagepusher:${data.kubernetes_secret.product-harbor-creds[0].data.AUTH}",
+      "robot$imagepusher:${data.kubernetes_secret.product-harbor-creds[0] ? data.kubernetes_secret.product-harbor-creds[0].data.AUTH : \"\"}",
     )   
-    enable_harbor = var.enable_harbor
-  }
-}
-
-data "template_file" "artifactory_dockercfg" {
-  count = var.enable_harbor ? 0 : 1
-  template = file("${path.module}/dockercfg.tpl")
-
-  vars = {
-    email = "jenkins@liatr.io"
-    artifactory_url   = "https://artifactory.toolchain.${var.cluster_domain}/docker-registry/${var.product_name}"
-    artifactory_auth = base64encode(
-      "${data.kubernetes_secret.jenkins_artifactory_credential[0].data.username}:${data.kubernetes_secret.jenkins_artifactory_credential[0].data.password}",
-    )
     enable_harbor = var.enable_harbor
   }
 }
@@ -38,7 +23,7 @@ resource "kubernetes_secret" "jenkins_repository_dockercfg" {
   }
  
   data = { 
-    "config.json" = var.enable_harbor ? data.template_file.multi_dockercfg[0].rendered : data.template_file.artifactory_dockercfg[0].rendered
+    "config.json" = data.template_file.dockercfg[0].rendered
   }
  
   type = "Opaque"
