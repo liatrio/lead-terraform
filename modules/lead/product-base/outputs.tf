ba0d7759cc45acc8c3927bb0ccccffbe90985232
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2020-02-27 14:00:10 -0800

    fix outputs.tf in product-base

--- modules/lead/product-base/outputs.tf
+++ modules/lead/product-base/outputs.tf
@@ -15,9 +15,9 @@ output "production_service_account" {
 }
 
 output "ci_staging_role_name" {
-  value = module.ci_staging_role.name
+  value = kubernetes_role.ci_staging_role.name
 }
 
 output "ci_production_role_name" {
-  value = module.ci_production_role.name
+  value = kubernetes.ci_production_role.name
 }
