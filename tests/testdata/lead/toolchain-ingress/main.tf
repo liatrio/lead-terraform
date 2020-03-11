47244f83c7590333e8602a4b6b8f32389e4343f5
Chris Schreiber <chriss@liatrio.com>
2020-03-11 16:58:53 -0700

    change test helm provider to version 3

--- tests/testdata/lead/toolchain-ingress/main.tf
+++ tests/testdata/lead/toolchain-ingress/main.tf
@@ -3,10 +3,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  version         = "0.10.4"
-  namespace       = var.namespace
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.15.1"
-  service_account = var.tiller_service_account
+  version         = "1.0.0"
 
   kubernetes {
     config_path            = var.kube_config_path
@@ -20,4 +17,5 @@ module "ingress" {
   issuer_name = var.issuer_name
   cluster_domain = var.cluster_domain
   crd_waiter = var.crd_waiter
+  ingress_controller_type = var.ingress_controller_type
 }
