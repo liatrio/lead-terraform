afe4c810cb6133715d7c65a20c7d9b5f0ea9dded
jallen2112 <jallen65@mail.csuchico.edu>
2020-03-17 14:50:10 -0700

    count indexing outputs.tf

--- modules/environment/aws/code-services/outputs.tf
+++ modules/environment/aws/code-services/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket" {
-  value =  aws_s3_bucket.code_services_bucket.id
+  value =  aws_s3_bucket.code_services_bucket[0].id
 }
 
 output "codebuild_role" {
-  value = aws_iam_role.codebuild_role.arn
+  value = aws_iam_role.codebuild_role[0].arn
 }
 
 output "codepipeline_role" {
-  value = aws_iam_role.codepipeline_role.arn
+  value = aws_iam_role.codepipeline_role[0].arn
 }
