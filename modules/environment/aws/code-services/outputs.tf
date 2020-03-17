919fffe9a663b2301ea8ba3b629b9a285acd8dad
jallen2112 <jallen65@mail.csuchico.edu>
2020-03-17 15:37:48 -0700

    fix outputs.tf in codeservices module

--- modules/environment/aws/code-services/outputs.tf
+++ modules/environment/aws/code-services/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket" {
-  value =  aws_s3_bucket.code_services_bucket[0].id
+  value =  length(aws_s3_bucket.code_services_bucket) > 0 ? aws_s3_bucket.code_services_bucket[0].id : ""
 }
 
 output "codebuild_role" {
-  value = aws_iam_role.codebuild_role[0].arn
+  value = length(aws_iam_role.codebuild_role) > 0 ? aws_iam_role.codebuild_role[0].arn : ""
 }
 
 output "codepipeline_role" {
-  value = aws_iam_role.codepipeline_role[0].arn
+  value = length(aws_iam_role.codepipeline_role) > 0 ? aws_iam_role.codepipeline_role[0].arn : ""
 }
