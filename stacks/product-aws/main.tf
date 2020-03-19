176de6201c1793665f1cd5200fed56815549f4ac
Michael Parker <michael@parker.gg>
2020-03-19 15:08:27 -0500

    use token to assume role?

--- stacks/product-aws/main.tf
+++ stacks/product-aws/main.tf
@@ -13,6 +13,8 @@ provider "aws" {
   version = ">= 2.29.0"
   region  = var.region
 
+  token = file("/var/run/secrets/eks.amazonaws.com/serviceaccount/token")
+
   assume_role {
     role_arn = var.role_arn
   }
