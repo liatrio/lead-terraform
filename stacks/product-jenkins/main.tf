6f066041c3bf4c17328409d003ff35cfc8f4b2d6
Takumi Nishida <takumin@liatrio.com>
2020-03-27 11:42:43 -0700

    changing versions back to 1.1.0

--- stacks/product-jenkins/main.tf
+++ stacks/product-jenkins/main.tf
@@ -11,7 +11,7 @@ provider "kubernetes" {
 
 provider "helm" {
   alias           = "toolchain"
-  version         = "1.0.0"
+  version         = "1.1.0"
 
   kubernetes {
     load_config_file = var.load_config_file
@@ -27,7 +27,7 @@ provider "kubernetes" {
 
 provider "helm" {
   alias           = "staging"
-  version         = "1.0.0"
+  version         = "1.1.0"
 
   kubernetes {
     load_config_file = var.load_config_file
@@ -43,7 +43,7 @@ provider "kubernetes" {
 
 provider "helm" {
   alias           = "production"
-  version         = "1.0.0"
+  version         = "1.1.0"
 
   kubernetes {
     load_config_file = var.load_config_file
@@ -59,7 +59,7 @@ provider "kubernetes" {
 
 provider "helm" {
   alias           = "system"
-  version         = "1.0.0"
+  version         = "1.1.0"
 
   kubernetes {
     load_config_file = var.load_config_file
