89e1a411e341b58d33c47d11781b146b7309ff02
jallen2112 <jallen65@mail.csuchico.edu>
2020-03-30 16:14:42 -0700

    fix typo

--- modules/lead/product-jenkins/harbor.tf
+++ modules/lead/product-jenkins/harbor.tf
@@ -6,7 +6,7 @@ resource "harbor_project" "project" {
 resource "harbor_robot_account" "robot" {
   count      = var.enable_harbor ? 1 : 0
   name       = "robot$imagepusher"
-  project_id = harbor_project[0].project.id
+  project_id = harbor_project[0].id
   robot_account_access {
     resource = "image"
     action   = "pull"
