cd439164e5ee588718a3eb8442996f85aa1d0de1
jallen2112 <jallen65@mail.csuchico.edu>
2020-03-31 11:24:13 -0700

    expand ci_staging_role to account for deployments.apps

--- modules/lead/product-base/staging.tf
+++ modules/lead/product-base/staging.tf
@@ -114,7 +114,7 @@ resource "kubernetes_role" "ci_staging_role" {
   }
 
   rule {
-    api_groups = ["", "extensions"]
+    api_groups = ["", "extensions", "apps"]
     resources  = ["*"]
     verbs      = ["*"]
   }
