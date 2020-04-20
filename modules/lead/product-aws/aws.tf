2b08f61bfd565f92b7d7283b2e8b6f795ad79c75
Takumi Nishida <takumin@liatrio.com>
2020-04-20 09:21:45 -0700

    just domains

--- modules/lead/product-aws/aws.tf
+++ modules/lead/product-aws/aws.tf
@@ -108,7 +108,7 @@ resource "aws_codebuild_project" "codebuild_staging" {
     }
     environment_variable {
       name  = "ISTIO_DOMAIN"
-      value = "${var.product_name}-${each.value.repo}-staging.apps.${var.cluster_domain}"
+      value = "staging.apps.${var.cluster_domain}"
     }
     environment_variable {
       name  = "PRODUCT_NAME"
@@ -174,7 +174,7 @@ resource "aws_codebuild_project" "codebuild_production" {
     }
     environment_variable {
       name  = "ISTIO_DOMAIN"
-      value = "${var.product_name}-${each.value.repo}-prod.apps.${var.cluster_domain}"
+      value = "prod.apps.${var.cluster_domain}"
     }
     environment_variable {
       name  = "PRODUCT_NAME"
