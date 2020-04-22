613d9804fc4ce0d0433010d9f380b812d6e15ee9
Michael Parker <michael@parker.gg>
2020-04-22 11:54:33 -0500

    fix tests

--- tests/testdata/tools/sdm/main.tf
+++ tests/testdata/tools/sdm/main.tf
@@ -22,10 +22,17 @@ module "sdm" {
   slack_client_signing_secret = var.slack_client_signing_secret
   enable_aws_event_mapper     = false
   toolchain_image_repo        = "489130170427.dkr.ecr.us-east-1.amazonaws.com"
-  operators                   = ["toolchain", "elasticsearch", "slack", "jenkins"]
-  product_types               = ["product-jenkins"]
+  operators                   = [
+    "toolchain",
+    "elasticsearch",
+    "slack",
+    "jenkins"
+  ]
+  product_types               = [
+    "product-jenkins"
+  ]
 
-  product_vars = {
+  product_vars      = {
     enable_keycloak        = "TEST"
     builder_images_version = "TEST"
     jenkins_image_version  = "TEST"
@@ -34,9 +41,13 @@ module "sdm" {
     enable_harbor          = "TEST"
     enable_artifactory     = "TEST"
 
-    s3_bucket         = "TEST"
-    codebuild_role    = "TEST"
-    codepipeline_role = "TEST"
-    codebuild_user    = "TEST"
+    aws_environment             = "TEST"
+    codebuild_security_group_id = "TEST"
+    s3_bucket                   = "TEST"
+    codebuild_role              = "TEST"
+    codepipeline_role           = "TEST"
+    codebuild_user              = "TEST"
   }
+  ecr_image_repo    = "TEST"
+  harbor_image_repo = "TEST"
 }
