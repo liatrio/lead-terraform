af538da2b0013f3b31b36617f32dee23ad31c913
Takumi Nishida <takumin@liatrio.com>
2020-04-23 14:07:47 -0700

    make if case dependent on codeservices flag

--- modules/environment/aws/eks/iam.tf
+++ modules/environment/aws/eks/iam.tf
@@ -96,6 +96,49 @@ resource "aws_iam_role_policy_attachment" "workspace_role_attachment" {
 }
 
 resource "aws_iam_role_policy" "workspace_role_policy" {
+  count = var.enable_aws_code_services ? 0 : 1
+  name = "workspace_access"
+  role = aws_iam_role.workspace_role.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "eks:DescribeCluster"
+      ],
+      "Effect": "Allow",
+      "Resource": "${module.eks.cluster_arn}"
+    },
+    {
+      "Action": [
+        "ec2:DescribeInstances","ec2:DescribeVolumesModifications"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "codecommit:GitPush", "codecommit:GitPull"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "ec2:ModifyVolume"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "workspace_role_policy_codeservices" {
+  count = var.enable_aws_code_services ? 1 : 0
   name = "workspace_access"
   role = aws_iam_role.workspace_role.name
 
