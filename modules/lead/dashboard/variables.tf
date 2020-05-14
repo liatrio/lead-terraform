50f4391b351178b17177b8139eddb32767c05902
Michael Parker <michael@parker.gg>
2020-05-14 14:57:09 -0500

    update dashboard to remove everything

--- modules/lead/dashboard/variables.tf
+++ modules/lead/dashboard/variables.tf
@@ -1,41 +1,6 @@
 variable "enabled" {
   default = true
 }
-variable "local" {
-  default = false
-}
 
-variable "root_zone_name" {
-}
-
-variable "cluster" {
-}
-
-variable "cluster_domain" {
-}
-
-variable "namespace" {
-}
-
-variable "dashboard_version" {
-}
-
-variable "enable_keycloak" {
-  default = false
-}
-
-variable "keycloak_realm_id" {
-}
-
-variable "crd_waiter" {
-}
-
-variable "k8s_storage_class" {}
-
-variable "elasticsearch_replicas" {
-  default = 3
-}
-
-variable "keycloak_admin_credential_secret" {}
-
-variable "toolchain_namespace" {}
+variable "namespace" {}
+variable "dashboard_version" {}
