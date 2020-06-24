fa04a6838ee14bb1d9bf9141ce3245d6261a80e3
Michael Parker <michael@parker.gg>
2020-06-24 15:14:57 -0500

    add vault administrator policy and auth

--- stacks/config/sharedsvc/vault.tf
+++ stacks/config/sharedsvc/vault.tf
@@ -11,6 +11,19 @@ resource "vault_auth_backend" "aws" {
   type = "aws"
 }
 
+resource "vault_aws_auth_backend_role" "vault_admin" {
+  backend                         = vault_auth_backend.aws.path
+  role                            = "vault-admin"
+  auth_type                       = "iam"
+  bound_iam_principal_arns        = [
+    "arn:aws:iam::265560927720:role/VaultAdministrator"
+  ]
+  resolve_aws_unique_ids = false
+  token_policies = [
+    vault_policy.vault_admin.name
+  ]
+}
+
 resource "vault_aws_auth_backend_role" "aws_admin" {
   backend                         = vault_auth_backend.aws.path
   role                            = "aws-admin"
