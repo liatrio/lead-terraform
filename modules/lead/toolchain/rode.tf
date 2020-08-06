c50208bd32834cf7446131e8147f485c89227999
gesparza3 <esparzagrant@gmail.com>
2020-08-06 14:53:20 -0700

    Use native templatefile function rather than template_file data source

--- modules/lead/toolchain/rode.tf
+++ modules/lead/toolchain/rode.tf
@@ -37,17 +37,6 @@ data "helm_repository" "liatrio-harbor" {
   url  =  "https://harbor.toolchain.lead.prod.liatr.io/chartrepo/public"
 }
 
-data "template_file" "rode_values" {
-  template = file("${path.module}/rode-values.tpl")
-
-  vars = {
-    iam_arn      = var.rode_service_account_arn
-    grafeas_cert = module.grafeas_certificate.cert_name
-    rode_cert    = module.rode_certificate.cert_name
-    ingress_hostname     = "rode.${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}
-  }
-}
-
 resource "helm_release" "rode" {
   count      = var.enable_rode ? 1 : 0
   repository = data.helm_repository.liatrio-harbor.metadata[0].name
@@ -56,6 +45,14 @@ resource "helm_release" "rode" {
   chart      = "rode"
   namespace  = var.namespace
 
+  values    = [
+    templatefile("${path.module}/rode-values.tpl", {
+      iam_arn      = var.rode_service_account_arn
+      grafeas_cert = module.grafeas_certificate.cert_name
+      rode_cert    = module.rode_certificate.cert_name
+      ingress_hostname     = "rode.${module.toolchain_namespace.name}.${var.cluster}.${var.root_zone_name}"
+    })
+  ]
   values = [
     data.template_file.rode_values.rendered
   ]
