45707b7fc95982b5e09964f79dbb1ae6968bec5a
Michael Parker <michael@parker.gg>
2020-08-26 16:40:06 -0500

    adds openstack secret mount for openstack-rancher-poc (#348)

---
+++ stacks/config/sharedsvc/vault-openstack.tf
@@ -0,0 +1,13 @@
+resource "vault_mount" "openstack" {
+  path        = "/openstack"
+  type        = "kv"
+
+  options = {
+    version = "2"
+  }
+}
+
+resource "vault_policy" "openstack_aws_admin" {
+  name   = "openstack-aws-admin"
+  policy = file("${path.module}/vault-policies/openstack-aws-admin.hcl")
+}
