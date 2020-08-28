45af8dc12fb8fa4b1f1c9bc0fa4c71863d6c2756
gesparza3 <esparzagrant@gmail.com>
2020-08-28 13:04:57 -0700

    Added descriptions to output and input variables

--- stages/apps/lead/outputs.tf
+++ stages/apps/lead/outputs.tf
@@ -1,9 +1,11 @@
 output "toolchain_namespace" {
   value = var.toolchain_namespace
+  description = "LEAD Toolchain namespace where applications are installed"
 }
 
 output "keycloak_hostname" {
   value = module.keycloak.keycloak_hostname
+  description = "Keycloak instance url to be used by configuration provider"
 }
 
 output "harbor_hostname" {
