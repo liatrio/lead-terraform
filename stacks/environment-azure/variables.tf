bbd9683467a149218493bd2a79c4cc3d1587c350
jgetz <30835090+jgetzie@users.noreply.github.com>
2020-09-01 17:05:43 -0500

    updates for VM creation

---
+++ stacks/environment-azure/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group" {
+  description = "Azure resource group name"
+  default     = "lead"
+}
+
+variable "prefix" {
+  description = "network name prefix"
+  default     = "lead"
+}
+
+variable "environment" {
+  description = "environment name"
+  default     = "sandbox"
+}
