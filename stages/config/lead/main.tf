8c5d47b5b6bde66e922a6c9ec934592612c2ddf6
Chris Schreiber <chriss@liatrio.com>
2020-09-02 19:39:12 -0700

    add k8s provider to config/lead stage

--- stages/config/lead/main.tf
+++ stages/config/lead/main.tf
@@ -10,6 +10,32 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.aws_eks_cluster.cluster.name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  version = "1.1.1"
+
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+}
+
 provider "vault" {
   address = var.vault_address
 
