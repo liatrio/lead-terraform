b0e2c4f7676bffec98d8ce0fa5b3b3de72f3e140
michael.begotner <michael.begotner@assurant.com>
2020-09-04 15:34:44 -0500

    removed unneeded outputs

--- stages/cloud-provider/azure/lead/outputs.tf
+++ stages/cloud-provider/azure/lead/outputs.tf
@@ -5,27 +5,3 @@ output "cluster_name" {
 output "resource_group_name" {
   value = var.resource_group_name
 }
-
-output "id" {
-  value = azurerm_kubernetes_cluster.main.id
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.main.kube_config_raw
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.main.kube_config.0.client_key
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.main.kube_config.0.host
-}
