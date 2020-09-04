d58bc7c4f53e00e5813273647e06b3990296e843
jgetz <30835090+jgetzie@users.noreply.github.com>
2020-09-04 13:21:14 -0500

    added version to azurerm provider

--- stages/apps/azure-example/main.tf
+++ stages/apps/azure-example/main.tf
@@ -3,6 +3,7 @@ terraform {
 }
 
 provider "azurerm" {
+  version                    = "=2.20.0"
   features {}
 }
 
--- stages/cloud-provider/azure/lead/main.tf
+++ stages/cloud-provider/azure/lead/main.tf
@@ -3,5 +3,6 @@ terraform {
 }
 
 provider "azurerm" {
+  version                    = "=2.20.0"
   features {}
 }
