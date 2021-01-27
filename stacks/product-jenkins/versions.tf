dc404a3950a5bf99ef74821c678614ed93e1fcba
Brad <bradf@liatrio.com>
2021-01-27 15:07:12 -0800

    Pinning helm version to 2.0.2

--- stacks/product-jenkins/versions.tf
+++ stacks/product-jenkins/versions.tf
@@ -3,8 +3,12 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "= 2.0.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.0.2"
     }
   }
 }
