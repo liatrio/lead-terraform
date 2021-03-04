63702a0e84763bf38dcd045b374e37dbee302b1f
Paul Henson <paulh@liatrio.com>
2021-03-04 10:50:05 -0800

    Added keycloak required provider

--- modules/lead/product-jenkins/versions.tf
+++ modules/lead/product-jenkins/versions.tf
@@ -10,16 +10,8 @@ terraform {
       source = "hashicorp/helm"
     }
     keycloak = {
-      # TF-UPGRADE-TODO
-      #
-      # No source detected for this provider. You must add a source address
-      # in the following format:
-      #
-      # source = "your-registry.example.com/organization/keycloak"
-      #
-      # For more information, see the provider source documentation:
-      #
-      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+      source = "mrparkers/keycloak"
+      version = "= 2.0.0-rc.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
