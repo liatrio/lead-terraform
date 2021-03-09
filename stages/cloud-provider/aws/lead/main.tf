e004595c6b726e444b175de21115a298f32e7776
Michael Parker <michael@parker.gg>
2021-03-09 12:00:02 -0600

    remove k8s provider in cloud-provider tf stage

--- stages/cloud-provider/aws/lead/main.tf
+++ stages/cloud-provider/aws/lead/main.tf
@@ -7,18 +7,4 @@ provider "aws" {
   region  = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
 data "aws_caller_identity" "current" {}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
-}
