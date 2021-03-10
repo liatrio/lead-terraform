969ba259191515bb9ca418d0191046d833f2fccc
PaulDHenson <PaulHenson.Tech@Gmail.com>
2021-03-10 13:57:20 -0800

    Terraform 14 upgrade (#435)
    * Added keycloak required provider
    
    * Upgraded to v0.13.6
    
    * Added providers to stacks product-jenkins versions file for upgrade
    
    * add permissions to read all objects in s3 bucket
    
    * Update stacks/product-jenkins/versions.tf
    
    Co-authored-by: Michael Parker <michael@parker.gg>
    
    * bumped keycloak version to 2.3.0
    
    Co-authored-by: Paul Henson <paulh@liatrio.com>
    Co-authored-by: jallen2112 <jallen65@mail.csuchico.edu>
    Co-authored-by: Michael Parker <michael@parker.gg>

--- modules/lead/product-jenkins/versions.tf
+++ modules/lead/product-jenkins/versions.tf
@@ -1,10 +1,26 @@
 
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 0.13"
   required_providers {
     harbor = {
-      source = "liatrio/harbor"
+      source  = "liatrio/harbor"
       version = "= 0.3.3"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+    keycloak = {
+      source = "mrparkers/keycloak"
+      version = "= 2.3.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
     }
   }
 }
