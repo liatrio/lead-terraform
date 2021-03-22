3fdf0755ab1688b9d0f922ca7624f84e8b839563
Grant Esparza <esparzagrant@gmail.com>
2021-03-22 16:10:42 -0700

    Fix vault key name

--- stages/apps/lead/artifactory.tf
+++ stages/apps/lead/artifactory.tf
@@ -1,5 +1,5 @@
-data "vault_generic_secret" "artifactory" {
-  path = "lead/aws/${data.aws_caller_identity.current.account_id}/artifactory"
+data "vault_generic_secret" "artifactory_jcr" {
+  path = "lead/aws/${data.aws_caller_identity.current.account_id}/artifactory-jcr"
 }
 
 module "artifactory_jcr" {
@@ -8,5 +8,5 @@ module "artifactory_jcr" {
   count               = var.enable_artifactory_jcr ? 1 : 0
   namespace           = var.toolchain_namespace
   hostname            = "artifactory-jcr.${var.toolchain_namespace}.${var.cluster_name}.${var.root_zone_name}"
-  jcr_admin_password  = data.vault_generic_secret.artifactory.data["admin-password"]
+  jcr_admin_password  = data.vault_generic_secret.artifactory_jcr.data["admin-password"]
 }
