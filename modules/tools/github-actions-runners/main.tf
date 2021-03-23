1655dd4376387cbace993fed9c918ef6197e5603
Steven O <steveno@liatrio.com>
2021-03-23 17:54:25 -0400

    adding path variable

--- modules/tools/github-actions-runners/main.tf
+++ modules/tools/github-actions-runners/main.tf
@@ -1,6 +1,6 @@
 resource helm_release github_runner_controller {
   name      = var.release_name
-  chart     = "./github-actions-runners"
+  chart     = "${path.module}/github-actions-runners"
   namespace = var.namespace
   wait      = true
 
