91c832eb05b3f2ac9f6c23da6724ddc3718e32bc
Brent Wolfram <brent.wolfram@gmail.com>
2021-03-24 06:40:51 -0400

    typo in variable definition

--- stages/apps/lead/velero.tf
+++ stages/apps/lead/velero.tf
@@ -7,5 +7,5 @@ module "velero" {
   bucket_name         = var.velero_bucket_name
   region              = var.region
   velero_aws_access_key_id = var.velero_aws_access_key_id
-  velero_aws_secret_access_key = velero_aws_secret_access_key
+  velero_aws_secret_access_key = var.velero_aws_secret_access_key
 }
