1548ed92407dd6f1f052ea1dd64e800c673d6c15
Brent Wolfram <brent.wolfram@gmail.com>
2021-03-24 06:28:39 -0400

    fix feature flags in outputs

--- modules/environment/aws/velero/outputs.tf
+++ modules/environment/aws/velero/outputs.tf
@@ -1,11 +1,11 @@
 output "velero_bucket_name" {
-  value = aws_s3_bucket.velero.0.bucket
+  value = length(aws_s3_bucket.velero) > 0 ? aws_s3_bucket.velero.0.bucket : ""
 }
 
 output "velero_aws_secret_access_key" {
-  value = aws_iam_access_key.velero.0.secret
+  value = length(aws_iam_access_key.velero) > 0 ? aws_iam_access_key.velero.0.secret : ""
 }
 
 output "velero_aws_access_key_id" {
-  value = aws_iam_access_key.velero.0.id
+  value = length(aws_iam_access_key.velero) > 0 ? aws_iam_access_key.velero.0.id : ""
 }
