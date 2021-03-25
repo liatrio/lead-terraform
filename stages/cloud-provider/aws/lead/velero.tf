5d4bc5760d21df88abf43b65a96f5a36a773f1cd
Brent Wolfram <brent.wolfram@gmail.com>
2021-03-25 07:46:36 -0400

    feature flag fix on velero module

--- stages/cloud-provider/aws/lead/velero.tf
+++ stages/cloud-provider/aws/lead/velero.tf
@@ -3,7 +3,6 @@ module "velero" {
   source = "../../../../modules/environment/aws/velero"
 
   velero_user   = var.velero_user
-  enable_velero = var.enable_velero
   cluster_name  = var.cluster_name
   account_id    = data.aws_caller_identity.current.account_id
 }
