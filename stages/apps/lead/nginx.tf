e43c1e184e1252b7d96f95e3a7f24dd3a1920998
Michael Parker <michael@parker.gg>
2021-03-29 10:02:27 -0500

    deploy ingress controllers for app traffic (#450)

---
+++ stages/apps/lead/nginx.tf
@@ -0,0 +1,10 @@
+# create an ingress controller for handling application traffic if istio is not installed
+module "app_nginx" {
+  count  = var.enable_istio ? 0 : 1
+  source = "../../../modules/lead/app-ingress"
+
+  cluster_domain = "${var.cluster_name}.${var.root_zone_name}"
+  issuer_name    = module.cluster_issuer.issuer_name
+  issuer_kind    = module.cluster_issuer.issuer_kind
+  namespace      = module.toolchain_namespace.name
+}
