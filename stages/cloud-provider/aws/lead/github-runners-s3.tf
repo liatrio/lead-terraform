192384e467a6ec6de992d6e59c6c79fd2f68195e
JonRudy <jonr@liatrio.com>
2021-04-08 12:20:31 -0700

    passing eks module values needed for irsa polices

--- stages/cloud-provider/aws/lead/github-runners-s3.tf
+++ stages/cloud-provider/aws/lead/github-runners-s3.tf
@@ -1,5 +1,6 @@
 module "github-runners-s3" {
   source = "../../../../modules/environment/aws/github-runners-s3"
 
-  cluster = var.cluster_name
+  cluster_name = var.cluster_name
+  aws_iam_openid_connect_provider = module.eks.aws_iam_openid_connect_provider 
 }
