9dc8bf898eefc9dbf9086273f53731c384a0407f
Steven O <steven.oetting@gmail.com>
2021-08-02 15:14:41 -0400

    adding external ingress components

--- stages/apps/sharedsvc/issuers.tf
+++ stages/apps/sharedsvc/issuers.tf
@@ -2,6 +2,10 @@ data "aws_route53_zone" "public_internal_services_liatr_io" {
   name         = "${var.internal_cluster_domain}."
 }
 
+data "aws_route53_zone" "public_services_liatr_io" {
+  name         = "${var.cluster_domain}."
+}
+
 module "internal_services_cluster_issuer" {
   source        = "../../../modules/common/cert-issuer"
   namespace     = module.system_namespace.name
@@ -15,6 +19,25 @@ module "internal_services_cluster_issuer" {
 
   route53_dns_region      = var.region
   route53_dns_hosted_zone = data.aws_route53_zone.public_internal_services_liatr_io.zone_id
+
+  depends_on = [
+    module.cert_manager
+  ]
+}
+
+module "internal_services_cluster_issuer_external" {
+  source        = "../../../modules/common/cert-issuer"
+  namespace     = module.system_namespace.name
+  issuer_name   = "letsencrypt-dns"
+  issuer_kind   = "ClusterIssuer"
+  issuer_type   = "acme"
+  issuer_server = "https://acme-v02.api.letsencrypt.org/directory"
+
+  acme_solver       = "dns"
+  provider_dns_type = "route53"
+
+  route53_dns_region      = var.region
+  route53_dns_hosted_zone = data.aws_route53_zone.public_services_liatr_io.zone_id
 
   depends_on = [
     module.cert_manager
--- stages/apps/sharedsvc/nginx.tf
+++ stages/apps/sharedsvc/nginx.tf
@@ -19,9 +19,29 @@ module "wildcard" {
   issuer_kind = module.internal_services_cluster_issuer.issuer_kind
 }
 
+module "wildcard_external" {
+  source = "../../../modules/common/certificates"
+
+  name      = "external-services-wildcard"
+  namespace = module.nginx_ingress_namespace.name
+  domain    = var.cluster_domain
+  enabled   = true
+
+  issuer_name = module.internal_services_cluster_issuer_external.issuer_name
+  issuer_kind = module.internal_services_cluster_issuer_external.issuer_kind
+}
+
 module "nginx" {
   source              = "../../../modules/tools/nginx"
   default_certificate = "${module.nginx_ingress_namespace.name}/${module.wildcard.cert_secret_name}"
   internal            = true
   namespace           = module.nginx_ingress_namespace.name
+}
+
+module "nginx_external" {
+  source              = "../../../modules/tools/nginx"
+  default_certificate = "${module.nginx_ingress_namespace.name}/${module.wildcard.cert_secret_name}"
+  internal            = false
+  namespace           = module.nginx_ingress_namespace.name
+  ingress_class       = "nginx-external"
 }
