66820197f7f12e843f2146aff68980a0ad3a00e9
Steven O <steven.oetting@gmail.com>
2021-08-03 15:59:00 -0400

    adding vault details

--- stages/apps/sharedsvc/sonarqube.tf
+++ stages/apps/sharedsvc/sonarqube.tf
@@ -1,5 +1,5 @@
 data "vault_generic_secret" "sonarqube" {
-  path = "lead/aws/${data.aws_caller_identity.current.account_id}/sonarqube"
+  path = "lead/aws/265560927720/sonarqube"
 }
 
 module "sonarqube_namespace" {
@@ -10,8 +10,8 @@ module "sonarqube_namespace" {
 module "sonarqube" {
   source = "../../../modules/tools/sonarqube"
 
-  admin_password    = data.vault_generic_secret.sonarqube.data["admin_password"]
-  postgres_password = data.vault_generic_secret.sonarqube.data["postgres_password"]
+  admin_password    = data.vault_generic_secret.sonarqube.data["admin"]
+  postgres_password = data.vault_generic_secret.sonarqube.data["postgres"]
   namespace         = module.sonarqube_namespace.name
   ingress_enabled   = true
   ingress_hostname  = "sonarqube.${var.cluster_domain}"
