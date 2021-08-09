f341c90c01be7d2ae686fd62bfb10ec678799bd8
Steven O <steven.oetting@gmail.com>
2021-08-09 15:41:15 -0400

    moved data source to main.tf

--- stages/apps/sharedsvc/main.tf
+++ stages/apps/sharedsvc/main.tf
@@ -11,6 +11,8 @@ data "aws_eks_cluster" "cluster" {
   name = var.eks_cluster_id
 }
 
+data "aws_caller_identity" "current" {}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
--- stages/apps/sharedsvc/sonarqube.tf
+++ stages/apps/sharedsvc/sonarqube.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 data "vault_generic_secret" "sonarqube" {
   path = "lead/aws/${data.aws_caller_identity.current.account_id}/sonarqube"
 }
