a1edcfacf95fcdf5b1222c23ba52d7a5a8cad53d
Steven O <steven.oetting@gmail.com>
2021-08-12 12:08:37 -0400

    added github runner config to cloud-provider

--- stages/cloud-provider/aws/sharedsvc/variables.tf
+++ stages/cloud-provider/aws/sharedsvc/variables.tf
@@ -75,3 +75,9 @@ variable "cluster_domain" {
 variable "internal_cluster_domain" {
   default = "internal.services.liatr.io"
 }
+
+variable "github_runner_service_accounts" {
+  description = "a list of service accounts to apply IRSA trust policy to. each service account should be in the format `namespace:name`"
+  type        = list(string)
+  default     = []
+}
