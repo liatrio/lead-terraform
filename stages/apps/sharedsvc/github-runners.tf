e42ce79f7c2dd19b643cc67140c75dd5d45792c7
Steven O <steven.oetting@gmail.com>
2021-08-12 12:22:07 -0400

    adding github runner config

---
+++ stages/apps/sharedsvc/github-runners.tf
@@ -0,0 +1,39 @@
+data "vault_generic_secret" "github_runner_app" {
+  for_each = var.github_runner_controllers
+
+  path = "lead/aws/${data.aws_caller_identity.current.account_id}/${each.value.vault_name}"
+}
+
+module "github_runner_controller" {
+  for_each = var.github_runner_controllers
+
+  source = "../../../modules/tools/github-actions-runner-controller"
+
+  namespace = each.value.namespace
+
+  github_app_id               = data.vault_generic_secret.github_runner_app[each.key].data["github_app_id"]
+  github_app_installation_id  = data.vault_generic_secret.github_runner_app[each.key].data["github_app_installation_id"]
+  github_app_private_key      = data.vault_generic_secret.github_runner_app[each.key].data["github_app_private_key"]
+  github_webhook_secret_token = data.vault_generic_secret.github_runner_app[each.key].data["github_webhook_secret_token"]
+  github_webhook_annotations  = { "kubernetes.io/ingress.class" : "toolchain-nginx" }
+
+  github_org     = each.value.github_org
+  ingress_domain = var.cluster_domain
+
+  depends_on = [module.cert_manager]
+}
+
+module "github_runners" {
+  for_each = var.github_runners
+
+  source     = "../../../modules/tools/github-actions-runners"
+  github_org = each.value.github_org
+  namespace  = each.value.namespace
+  image      = each.value.image
+  labels     = each.value.labels
+  github_runners_service_account_annotations = {
+    "eks.amazonaws.com/role-arn" = var.github_runners_service_account_arn
+  }
+
+  depends_on = [module.github_runner_controller]
+}
