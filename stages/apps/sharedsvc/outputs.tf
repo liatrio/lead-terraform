714b9a2110e54e8d182331f4b96db6529e9754e7
Steven O <steven.oetting@gmail.com>
2021-08-18 18:43:02 -0400

    rearranged outputs

--- stages/apps/sharedsvc/outputs.tf
+++ stages/apps/sharedsvc/outputs.tf
@@ -2,10 +2,6 @@ output "vault_hostname" {
   value = local.vault_hostname
 }
 
-output "sonarqube_hostname" {
-  value = local.sonarqube_hostname
-}
-
 output "keycloak_namespace" {
   value = module.keycloak_namespace.name
 }
@@ -14,14 +10,18 @@ output "keycloak_realm" {
   value = local.keycloak_realm
 }
 
-output "sonar_keycloak_client_id" {
-  value = local.sonar_keycloak_client_id
-}
-
 output "keycloak_issuer_uri" {
   value = local.keycloak_issuer_uri
 }
 
 output "keycloak_hostname" {
   value = module.keycloak.keycloak_hostname
+}
+
+output "sonar_keycloak_client_id" {
+  value = local.sonar_keycloak_client_id
+}
+
+output "sonarqube_hostname" {
+  value = local.sonarqube_hostname
 }
