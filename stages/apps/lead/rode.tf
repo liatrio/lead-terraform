3b6ff2992b0f9807ba7d032d8afa4bcc4894f16d
Steven O <steven.oetting@gmail.com>
2021-08-19 18:25:03 -0400

    partially fixed module call from lead

--- stages/apps/lead/rode.tf
+++ stages/apps/lead/rode.tf
@@ -5,6 +5,8 @@ module "rode" {
 
   namespace                = var.toolchain_namespace
   rode_service_account_arn = var.rode_service_account_arn
-  ingress_domain           = "${var.cluster_name}.${var.root_zone_name}"
+  rode_ingress_hostname          = "rode.${var.toolchain_namespace}.${var.cluster_name}.${var.root_zone_name}"
 
+  grafeas_elasticsearch_password = data.vault_generic_secret.keycloak.data["FIX_ME"]
+  grafeas_elasticsearch_username = data.vault_generic_secret.keycloak.data["FIX_ME"]
 }
