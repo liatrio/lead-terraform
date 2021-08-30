6cdc86844a1749d4b4d41d40f8791083a2a87bb1
Connor Mulligan <misterc500@gmail.com>
2021-08-30 11:05:36 -0700

    fix: update keycloak_hostname in output.tf

--- stages/apps/lead/outputs.tf
+++ stages/apps/lead/outputs.tf
@@ -4,7 +4,7 @@ output "toolchain_namespace" {
 }
 
 output "keycloak_hostname" {
-  value       = module.keycloak[0].keycloak_hostname
+  value       =  var.enable_keycloak ? module.keycloak[0].keycloak_hostname : ""
   description = "Keycloak instance url to be used by configuration provider"
 }
 
