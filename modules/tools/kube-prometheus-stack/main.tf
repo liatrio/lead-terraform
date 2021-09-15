b6643e962408b9112fc0d9d21380c60f28c9a74a
Connor Mulligan <misterc500@gmail.com>
2021-09-15 11:59:41 -0700

    add custom kubernetes-overcommit-max-nodes prometheus rule

--- modules/tools/kube-prometheus-stack/main.tf
+++ modules/tools/kube-prometheus-stack/main.tf
@@ -33,6 +33,54 @@ resource "helm_release" "kube_prometheus_stack" {
   ]
 }
 
+resource "kubernetes_manifest" "prometheus_rule_kube_overcommit-max-nodes" {
+  manifest = {
+    kind       = "PrometheusRule"
+    apiVersion = "monitoring.coreos.com/v1"
+    metadata = {
+      annotations = {
+        "meta.helm.sh/release-name"     = "kube-prometheus-stack"
+        "meta.helm.sh/release-namespace" = "monitoring"
+        "prometheus-operator-validated"    = "true"
+      }
+      labels = {
+        app                            = "kube-prometheus-stack"
+        "app.kubernetes.io/instance"   = "kube-prometheus-stack"
+        "app.kubernetes.io/managed-by" = "Helm"
+        "app.kubernetes.io/part-of"    = "kube-prometheus-stack"
+        "app.kubernetes.io/version"    = "18.0.3"
+        chart                          = "kube-prometheus-stack-18.0.3"
+        heritage                       = "Helm"
+        release                        = "kube-prometheus-stack"
+      }
+      name      = "kube-prometheus-stack-kubernetes-overcommit-max-nodes"
+      namespace = "monitoring"
+    }
+    spec = {
+      groups = [
+        {
+          name = "kubernetes-resources"
+          rules = [
+            {
+              alert = "KubeMemoryOvercommitWithMaxNodes"
+              annotations = {
+                description = "Cluster has overcommitted memory resource requests for Pods and cannot tolerate node failure."
+                runbook_url = "https://github.com/kubernetes-monitoring/kubernetes-mixin/tree/master/runbook.md#alert-name-kubememoryovercommit"
+                summary     = "Cluster has overcommitted memory resource requests."
+              }
+              expr = "sum(namespace_memory:kube_pod_container_resource_requests:sum{})\n  /\nsum(kube_node_status_allocatable{resource=\"memory\"})\n  \u003e\n((count(kube_node_status_allocatable{resource=\"memory\"}) \u003e 1) - 1)\n  /\ncount(kube_node_status_allocatable{resource=\"memory\"})"
+              for  = "5m"
+              labels = {
+                severity = "warning"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
 resource "kubernetes_manifest" "kube_prometheus_harbor_monitor" {
   manifest = {
     apiVersion = "monitoring.coreos.com/v1"
