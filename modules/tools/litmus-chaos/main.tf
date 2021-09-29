fff46229ee3faffbd1ed1852778185922c856bf6
Samantha Mathistad <ssmathistad@ip-10-0-0-193.ec2.internal>
2021-09-29 08:25:35 -0700

    feat: add litmus service account

--- modules/tools/litmus-chaos/main.tf
+++ modules/tools/litmus-chaos/main.tf
@@ -25,3 +25,67 @@ resource "helm_release" "litmus_kubernetes_chaos_experiments" {
   wait       = true
 }
 
+resource "kubernetes_service_account" "litmus_service_account" {
+  metadata {
+    name = "chaos-sa"
+    namespace = "litmus"
+    labels = {
+       name = "chaos-sa"
+    }
+  }
+}
+
+# apiVersion: v1
+# kind: ServiceAccount
+# metadata:
+#   name: chaos-sa
+#   namespace: litmus
+#   labels:
+#     name: pod-delete-sa
+
+# resource "kubernetes_manifest" "litmus_service_account" {
+#   manifest = {
+#     kind       = "PrometheusRule"
+#     apiVersion = "monitoring.coreos.com/v1"
+#     metadata = {
+#       annotations = {
+#         "meta.helm.sh/release-name"      = "kube-prometheus-stack"
+#         "meta.helm.sh/release-namespace" = "monitoring"
+#         "prometheus-operator-validated"  = "true"
+#       }
+#       labels = {
+#         app                            = "kube-prometheus-stack"
+#         "app.kubernetes.io/instance"   = "kube-prometheus-stack"
+#         "app.kubernetes.io/managed-by" = "Helm"
+#         "app.kubernetes.io/part-of"    = "kube-prometheus-stack"
+#         "app.kubernetes.io/version"    = "18.0.3"
+#         chart                          = "kube-prometheus-stack-18.0.3"
+#         heritage                       = "Helm"
+#         release                        = "kube-prometheus-stack"
+#       }
+#       name      = "kube-prometheus-stack-kubernetes-cluster-at-max-nodes"
+#       namespace = "monitoring"
+#     }
+#     spec = {
+#       groups = [
+#         {
+#           name = "kubernetes-resources"
+#           rules = [
+#             {
+#               alert = "KubeClusterAtMaxNodes"
+#               annotations = {
+#                 description = "Cluster has scaled to its maximum node capacity and cannot tolerate node failure."
+#                 summary     = "Cluster has reached its node limit."
+#               }
+#               expr = "count(kube_node_status_condition{condition=\"Ready\", status=\"true\"}) == 35"
+#               for  = "5m"
+#               labels = {
+#                 severity = "critical"
+#               }
+#             }
+#           ]
+#         }
+#       ]
+#     }
+#   }
+# }
