aaf2a073877318bf88930ec3dab8e301fc978abe
jknight-liatrio <joek@liatrio.com>
2021-11-05 15:33:09 -0500

    Refactor lead-environment pipeline iam to use module

--- stages/cloud-provider/aws/sharedsvc/github-runners.tf
+++ stages/cloud-provider/aws/sharedsvc/github-runners.tf
@@ -7,42 +7,13 @@ module "github-runners-s3" {
   aws_iam_openid_connect_provider_url = module.eks.aws_iam_openid_connect_provider_url
 }
 
-resource "aws_iam_role" "lead_pipelines_service_account" {
-  name = "${module.eks.cluster_id}-lead-pipelines-service-account"
+module "liatrio_lead_environment_pipelines" {
+  source = "../../../../modules/environment/aws/iam/github-runner-iam"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${module.eks.aws_iam_openid_connect_provider_arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${replace(module.eks.aws_iam_openid_connect_provider_url, "https://", "")}:sub": "system:serviceaccount:${var.github_runners_namespace}:liatrio-lead-environments-runners"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
-
-data "aws_iam_policy_document" "lead_pipelines_role_assume_role_policy" {
-  statement {
-    sid     = "LeadPipelinesAssumeRole"
-    actions = ["sts:AssumeRole"]
-
-    resources = var.lead_environments_pipeline_roles
-  }
-}
-
-resource "aws_iam_role_policy" "lead_pipeines" {
-  name = "${module.eks.cluster_id}-lead-pipelines"
-  role = aws_iam_role.lead_pipelines_service_account.name
-
-  policy = data.aws_iam_policy_document.lead_pipelines_role_assume_role_policy.json
+  name = "liatrio-lead-environments-pipelines"
+  service_account_name = "liatrio-lead-environments-runners"
+  aws_iam_openid_connect_provider_arn = module.eks.aws_iam_openid_connect_provider_arn
+  aws_iam_openid_connect_provider_url = module.eks.aws_iam_openid_connect_provider_url
+  namespace = var.github_runners_namespace
+  roles = var.lead_environments_pipeline_roles
 }
