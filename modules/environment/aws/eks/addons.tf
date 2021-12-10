17ac9d864b23ddb0d3b29e1c816856588598d44d
Michael Parker <michael@parker.gg>
2021-12-10 09:43:40 -0600

    fix: use cluster ID from eks module

--- modules/environment/aws/eks/addons.tf
+++ modules/environment/aws/eks/addons.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "coredns" {
-  cluster_name      = "lead"
+  cluster_name      = module.eks.cluster_id
   addon_name        = "coredns"
   addon_version     = "v1.7.0-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
