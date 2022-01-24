b38676c8fbfbd9492e5e27ac709c21e80327dda1
Michael Parker <michael@parker.gg>
2022-01-24 14:07:50 -0600

    feat: add s3-based static website for hosting helm charts

--- stages/cloud-provider/aws/sharedsvc/iam.tf
+++ stages/cloud-provider/aws/sharedsvc/iam.tf
@@ -7,19 +7,6 @@ module "cluster_autoscaler_iam" {
   openid_connect_provider_url = module.eks.aws_iam_openid_connect_provider_url
 }
 
-data "aws_route53_zone" "private_internal_services_liatr_io" {
-  name         = "${var.internal_cluster_domain}."
-  private_zone = true
-}
-
-data "aws_route53_zone" "public_internal_services_liatr_io" {
-  name = "${var.internal_cluster_domain}."
-}
-
-data "aws_route53_zone" "services_liatr_io" {
-  name = "${var.cluster_domain}."
-}
-
 module "external_dns_iam" {
   source = "../../../../modules/environment/aws/iam/external-dns"
 
