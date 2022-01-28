086fed1b937bc1eb21931c79127099c2f2250027
Michael Parker <michael@parker.gg>
2022-01-28 09:46:30 -0600

    fix: create deployer credentials for s3 helm repo (#581)

--- stages/cloud-provider/aws/sharedsvc/s3-helm.tf
+++ stages/cloud-provider/aws/sharedsvc/s3-helm.tf
@@ -5,6 +5,7 @@ locals {
 module "helm_s3_website" {
   source = "../../../../modules/environment/aws/s3-website"
 
-  domain          = local.helm_repository_url
-  route53_zone_id = data.aws_route53_zone.services_liatr_io.zone_id
+  domain                      = local.helm_repository_url
+  route53_zone_id             = data.aws_route53_zone.services_liatr_io.zone_id
+  create_deployer_credentials = true
 }
