b5f53bf37b7bccf663f3b362b7b2e4cca4eca0e6
Chris Schreiber <chriss@liatrio.com>
2022-05-26 10:42:10 -0700

    chore: remove unused code

--- modules/environment/aws/eks/iam.tf
+++ modules/environment/aws/eks/iam.tf
@@ -19,66 +19,6 @@ EOF
   permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/Developer"
 }
 
-# resource "aws_iam_policy" "workspace_role_boundary" {
-#   name        = "${var.cluster}-workspace_role_boundary"
-#   description = "Permission boundaries for workspace role"
-
-#   policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "NotAction": [
-#                 "iam:*",
-#                 "organizations:*",
-#                 "account:*"
-#             ],
-#             "Resource": "*"
-#         },
-#         {
-#             "Action": [
-#                 "iam:Get*",
-#                 "iam:List*",
-#                 "iam:CreateInstanceProfile",
-#                 "iam:DeleteInstanceProfile",
-#                 "iam:AddRoleToInstanceProfile",
-#                 "iam:RemoveRoleFromInstanceProfile",
-#                 "iam:CreatePolicy",
-#                 "iam:CreateServiceLinkedRole",
-#                 "iam:DeleteServiceLinkedRole",
-#                 "organizations:DescribeOrganization",
-#                 "account:ListRegions"
-#             ],
-#             "Effect": "Allow",
-#             "Resource": "*"
-#         },
-#         {
-#             "Action": [
-#                 "iam:CreateRole",
-#                 "iam:AttachRolePolicy",
-#                 "iam:PutRolePermissionsBoundary"
-#             ],
-#             "Effect": "Allow",
-#             "Condition": {
-#                 "StringEquals": {
-#                     "iam:PermissionsBoundary": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${var.cluster}-workspace_role_boundary}"
-#                 }
-#             },
-#             "Resource": "*"
-#         },
-#         {
-#             "Action": [
-#                 "iam:PassRole"
-#             ],
-#             "Effect": "Allow",
-#             "Resource": "*"
-#         }
-#     ]
-# }
-# EOF
-# }
-
 resource "aws_iam_role_policy_attachment" "workspace_role_attachment" {
   role       = aws_iam_role.workspace_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloud9User"
