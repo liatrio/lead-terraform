ed9eb5b70aafb5f9e75eb904c3bc30e25047fe89
Anthony Gauthier <antho325@hotmail.com>
2022-06-16 12:08:10 -0400

    style(product-base): tf fmt

--- modules/lead/product-aws/product-base.tf
+++ modules/lead/product-aws/product-base.tf
@@ -1,8 +1,8 @@
 module "product_base" {
-  source                  = "../product-base"
-  cluster_domain          = var.cluster_domain
-  product_name            = var.product_name
-  image_whitelist         = var.image_whitelist
+  source          = "../product-base"
+  cluster_domain  = var.cluster_domain
+  product_name    = var.product_name
+  image_whitelist = var.image_whitelist
 
   providers = {
     helm.staging          = helm.staging
