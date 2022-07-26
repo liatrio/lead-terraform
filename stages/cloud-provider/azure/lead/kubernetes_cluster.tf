d1a11d3bb2766327798b7c773f2c354df400977e
micahperez2 <micahperez99@gmail.com>
2022-07-26 14:02:39 -0600

    fix: remove tf ignore to run scanning

--- stages/cloud-provider/azure/lead/kubernetes_cluster.tf
+++ stages/cloud-provider/azure/lead/kubernetes_cluster.tf
@@ -1,6 +1,3 @@
-#tfsec:ignore:azure-container-use-rbac-permissions
-#tfsec:ignore:azure-container-logging
-#tfsec:ignore:azure-container-limit-authorized-ips
 resource "azurerm_kubernetes_cluster" "main" {
   name                = var.cluster_name
   location            = var.location
