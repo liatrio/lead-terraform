e0aea3875a761e9da2be9016ae1b5bf24df30dae
micahperez2 <micahperez99@gmail.com>
2022-08-02 16:09:04 -0700

    chore: run terraform fmt

--- stages/cloud-provider/aws/lead/s3-logging.tf
+++ stages/cloud-provider/aws/lead/s3-logging.tf
@@ -1,3 +1,6 @@
 module "s3-logging" {
-    source = "../../../../modules/environment/aws/s3-logging"
+  source = "../../../../modules/environment/aws/s3-logging"
+
+  cluster_name = var.cluster_name
+  account_id   = data.aws_caller_identity.current.account_id
 }
--- stages/cloud-provider/aws/lead/velero.tf
+++ stages/cloud-provider/aws/lead/velero.tf
@@ -2,7 +2,7 @@ module "velero" {
   count  = var.enable_velero ? 1 : 0
   source = "../../../../modules/environment/aws/velero"
 
-  cluster_name    = var.cluster_name
-  account_id      = data.aws_caller_identity.current.account_id
-  s3-logging-id   = var.s3-logging-id
+  cluster_name  = var.cluster_name
+  account_id    = data.aws_caller_identity.current.account_id
+  s3-logging-id = var.s3-logging-id
 }
