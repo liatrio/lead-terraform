1484af58d15d53df14f49c71c1e952fdcec7db44
jallen2112 <jallen65@mail.csuchico.edu>
2022-09-13 18:03:10 -0700

    fix sa name

--- stages/cloud-provider/aws/sharedsvc-onegheorg/github-runners.tf
+++ stages/cloud-provider/aws/sharedsvc-onegheorg/github-runners.tf
@@ -1,7 +1,7 @@
 module "onegheorg_pipeline_iam" {
   source                              = "../../../../modules/environment/aws/iam/github-runner-iam"
   name                                = "liatrio-onegheorg-pipeline"
-  service_account_name                = "liatrio-onegheorg-runners"
+  service_account_name                = "onegheorg-runners"
   aws_iam_openid_connect_provider_arn = "arn:aws:iam::265560927720:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5D373074AFAEC39576C6045E44C487CC"
   aws_iam_openid_connect_provider_url = "https://oidc.eks.us-east-1.amazonaws.com/id/5D373074AFAEC39576C6045E44C487CC"
   namespace                           = var.github_runners_namespace
