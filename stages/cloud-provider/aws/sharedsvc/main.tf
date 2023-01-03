4bb21b82b264e1536d9f64268ecc5ca82cbcaecf
luisvieirajs <luis.vieira@liatrio.com>
2023-01-03 16:24:57 -0500

    add sharedsvc aws_route53_zone_association with the internal_vpn

--- stages/cloud-provider/aws/sharedsvc/main.tf
+++ stages/cloud-provider/aws/sharedsvc/main.tf
@@ -21,6 +21,12 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
+data "aws_vpc" "internal_vpn_vpc" {
+  tags = {
+    Name = "internal-vpn-vpc"
+  }
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
@@ -50,4 +56,9 @@ resource "aws_route53_zone" "public_internal_services_liatr_io" {
 
 resource "aws_route53_zone" "services_liatr_io" {
   name = "${var.cluster_domain}."
+}
+
+resource "aws_route53_zone_association" "internal_vpn" {
+  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
+  vpc_id  = data.aws_vpc.internal_vpn_vpc.id
 }
