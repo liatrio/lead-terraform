5243c77e37def29fb3fa6e9adc0a74ebe5a1f465
luisvieirajs <luis.vieira@liatrio.com>
2023-01-04 17:59:50 -0500

    revert private_internal_services_liatr_io vpc

--- stages/cloud-provider/aws/sharedsvc/main.tf
+++ stages/cloud-provider/aws/sharedsvc/main.tf
@@ -44,6 +44,9 @@ data "aws_vpc" "shared_svc" {
 
 resource "aws_route53_zone" "private_internal_services_liatr_io" {
   name         = "${var.internal_cluster_domain}."
+  vpc {
+    vpc_id = data.aws_vpc.shared_svc.id
+  }
 }
 
 resource "aws_route53_zone" "public_internal_services_liatr_io" {
