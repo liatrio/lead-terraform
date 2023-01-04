9ab95b2d00fe393976d0121b1ce5d54380b96536
luisvieirajs <luis.vieira@liatrio.com>
2023-01-04 15:34:04 -0500

    remove certs outputs on green cluster

--- modules/common/certificates/outputs.tf
+++ modules/common/certificates/outputs.tf
@@ -1,15 +1,15 @@
-output "cert_status" {
-  value = var.enabled && length(helm_release.certificates) == 1 ? helm_release.certificates[0].status : ""
-}
-
-output "cert_name" {
-  value = var.name
-}
-
-output "cert_secret_name" {
-  value = "${var.name}-certificate"
-
-  depends_on = [
-    helm_release.certificates
-  ]
-}
+#output "cert_status" {
+#  value = var.enabled && length(helm_release.certificates) == 1 ? helm_release.certificates[0].status : ""
+#}
+#
+#output "cert_name" {
+#  value = var.name
+#}
+#
+#output "cert_secret_name" {
+#  value = "${var.name}-certificate"
+#
+#  depends_on = [
+#    helm_release.certificates
+#  ]
+#}
