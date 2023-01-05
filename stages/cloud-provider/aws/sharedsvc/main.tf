25cd41e0ad09772ed3e89a4c01883bc12f44db3c
luisvieirajs <luis.vieira@liatrio.com>
2023-01-05 17:31:30 -0500

    revert without vpn

--- stages/cloud-provider/aws/sharedsvc/main.tf
+++ stages/cloud-provider/aws/sharedsvc/main.tf
@@ -54,7 +54,7 @@ resource "aws_route53_zone" "private_internal_services_liatr_io" {
   name         = "${var.internal_cluster_domain}."
 
   vpc {
-    vpc_id = data.aws_vpc.shared_service_vpc.id
+    vpc_id = data.aws_vpc.shared_svc.id
   }
 }
 
@@ -66,12 +66,12 @@ resource "aws_route53_zone" "services_liatr_io" {
   name = "${var.cluster_domain}."
 }
 
-resource "aws_route53_zone_association" "internal_vpn" {
-  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
-  vpc_id  = data.aws_vpc.internal_vpn_vpc.id
-}
-
-resource "aws_route53_zone_association" "internal_shared_svc" {
-  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
-  vpc_id = data.aws_vpc.shared_svc.id
-}
+#resource "aws_route53_zone_association" "internal_vpn" {
+#  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
+#  vpc_id  = data.aws_vpc.internal_vpn_vpc.id
+#}
+#
+#resource "aws_route53_zone_association" "internal_shared_svc" {
+#  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
+#  vpc_id = data.aws_vpc.shared_svc.id
+#}
