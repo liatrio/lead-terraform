463110214da85e88b7a68a874432527d8ac12684
luisvieirajs <luis.vieira@liatrio.com>
2023-01-05 15:39:11 -0500

    sync zones sharedsvc green cluster with sharedsvc cluster zones from aws-terraform

--- stages/cloud-provider/aws/sharedsvc/main.tf
+++ stages/cloud-provider/aws/sharedsvc/main.tf
@@ -21,6 +21,12 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
+data "aws_vpc" "shared_service_vpc" {
+  tags = {
+    Name = "shared-service-cluster-vpc"
+  }
+}
+
 data "aws_vpc" "internal_vpn_vpc" {
   tags = {
     Name = "internal-vpn-vpc"
@@ -44,22 +50,47 @@ data "aws_vpc" "shared_svc" {
 
 resource "aws_route53_zone" "private_internal_services_liatr_io" {
   name         = "${var.internal_cluster_domain}."
+
+  vpc {
+    vpc_id = data.aws_vpc.shared_service_vpc.id
+  }
+
+  tags = {
+    Environment = "sharedsvc"
+    Client      = "liatrio"
+    Project     = "Network Infrastructure"
+    Owner       = "parker"
+    Provisioner = "terraform:liatrio/aws-terraform"
+    Private     = "true"
+  }
 }
 
 resource "aws_route53_zone" "public_internal_services_liatr_io" {
   name = "${var.internal_cluster_domain}."
+
+  tags = {
+    Environment = "sharedsvc"
+    Client      = "liatrio"
+    Project     = "Network Infrastructure"
+    Owner       = "parker"
+    Provisioner = "terraform:liatrio/aws-terraform"
+    Public      = "true"
+  }
 }
 
 resource "aws_route53_zone" "services_liatr_io" {
   name = "${var.cluster_domain}."
 }
 
+resource "aws_route53_record" "services_liatr_io_ns" {
+  zone_id = aws_route53_zone.services_liatr_io.zone_id
+  name    = "${var.internal_cluster_domain}."
+  type    = "NS"
+  ttl     = "300"
+  records = aws_route53_zone.public_internal_services_liatr_io.name_servers
+}
+
 resource "aws_route53_zone_association" "internal_vpn" {
   zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
   vpc_id  = data.aws_vpc.internal_vpn_vpc.id
-}
-
-resource "aws_route53_zone_association" "internal_shared_svc" {
-  zone_id = aws_route53_zone.private_internal_services_liatr_io.zone_id
-  vpc_id = data.aws_vpc.shared_svc.id
 }
